6d666ba8b7d68289004c856a80e6cf0df00a6cbe
Eusebio Suarez <eusebiosuaresmartines@gmail.com>
2025-09-04 10:29:11 -0500

    configuracion inicial

---
+++ src/test/java/com/example/SpringSecurirtyJWT/SpringSecurirtyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSecurirtyJWT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurirtyJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
