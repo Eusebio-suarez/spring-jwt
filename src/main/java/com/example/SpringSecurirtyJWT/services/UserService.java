08f812fa4dc38cbfff66ed8d9d98f8e667e7fd1e
eusebio suarez <eusebiosuaresmartines@gmail.com>
2025-09-06 22:36:28 -0500

    encriptar contraseñas con BCrypt

--- src/main/java/com/example/SpringSecurirtyJWT/services/UserService.java
+++ src/main/java/com/example/SpringSecurirtyJWT/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.SpringSecurirtyJWT.dto.request.UserRequestDTO;
@@ -16,6 +17,10 @@ import com.example.SpringSecurirtyJWT.repository.UserRepository;
 @Service
 public class UserService {
     
+    //encriptra contraseñas
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Autowired
     private UserRepository userRepository;
     
@@ -46,7 +51,8 @@ public class UserService {
         UserEntity user = UserEntity.builder()
                 .username(userRequest.getUserName())
                 .email(userRequest.getEmail())
-                .password(userRequest.getPassword())
+                //encriptar la contraseña que se va a guardar en la bd
+                .password(passwordEncoder.encode(userRequest.getPassword()))
                 .roles(roles)
                 .build();
 
